69b386f0530c128ff170bdff59db7463948ccf12
Jai Ram Rideout <jai.rideout@gmail.com>
2013-01-30 13:10:48 -0700

    Added alpha diversity glossary.

--- my_microbes/support_files/js/helpers.js
+++ my_microbes/support_files/js/helpers.js
@@ -73,10 +73,13 @@ function gg(targetq) {
   window.open("http://www.google.com/search?q=" + targetq, 'searchwin');
 }
 
+// Adds definition divs and click handlers for definition links.
 function initializeGlossary() {
-  otu_definition_html = "<div id='otu-definition' class='glossary-item' title='Operational Taxonomic Units (OTUs)'><p>An OTU is a functional definition of a taxonomic group, often based on percent identity of the 16S ribosomal RNA (rRNA) gene. In this study, we began with a reference collection of 16S rRNA sequences (derived from the <a href='http://greengenes.secondgenome.com/'>Greengenes database</a>), and each of those sequences was used to define an Opertational Taxonomic Unit. We then compared all of the sequence reads that we obtained in this study (from your microbial communities and everyone else's) to those reference OTUs, and if a sequence read matched one of those sequences at at least 97%% identity, the read was considered an observation of that reference OTU. This process is one strategy for <i>OTU picking</i>, or assigning sequence reads to OTUs.</p></div>";
+  var otu_definition_html = "<div id='otu-definition' class='glossary-item' title='Operational Taxonomic Units (OTUs)'><p>An OTU is a functional definition of a taxonomic group, often based on percent identity of the 16S ribosomal RNA (rRNA) gene. In this study, we began with a reference collection of 16S rRNA sequences (derived from the <a href='http://greengenes.secondgenome.com/'>Greengenes database</a>), and each of those sequences was used to define an Opertational Taxonomic Unit. We then compared all of the sequence reads that we obtained in this study (from your microbial communities and everyone else's) to those reference OTUs, and if a sequence read matched one of those sequences at at least 97%% identity, the read was considered an observation of that reference OTU. This process is one strategy for <i>OTU picking</i>, or assigning sequence reads to OTUs.</p></div>";
 
-  $("body").append(otu_definition_html);
+  var alpha_diversity_html = "<div id='adiv-definition' class='glossary-item' title='Alpha Diversity'><p>Alpha diversity refers to within-sample diversity, and can be a measure of the number of different types of organisms that are present in a sample (i.e., the richness of the sample), the shape of the distribution of counts of different organisms in a sample (i.e., the evenness of the sample), or some other property of a single sample. This is in contrast to beta diversity, which is a measure of between-sample diversity.";
+
+  $("body").append(otu_definition_html, alpha_diversity_html);
 
   $(".glossary-item").dialog({
     autoOpen: false,
@@ -90,6 +93,11 @@ function initializeGlossary() {
 
   $(".otus").click(function(event) {
     openDialog("otu-definition", event.target.id);
+    return false;
+  });
+
+  $(".adiv").click(function(event) {
+    openDialog("adiv-definition", event.target.id);
     return false;
   });
 }
