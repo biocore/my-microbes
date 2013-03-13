446124035d7109e0b418ef19bc9f7a3d1d03e6e8
Antonio Gonzalez <antgonza@gmail.com>
2013-03-13 16:41:11 -0600

    my-microbes 0.1.0 release

--- my_microbes/format.py
+++ my_microbes/format.py
@@ -5,7 +5,7 @@ __author__ = "John Chase"
 __copyright__ = "Copyright 2013, The QIIME project"
 __credits__ = ["John Chase", "Greg Caporaso", "Jai Ram Rideout"]
 __license__ = "GPL"
-__version__ = "0.0.0-dev"
+__version__ = "0.1.0"
 __maintainer__ = "John Chase"
 __email__ = "jc33@nau.edu"
 
--- tests/test_util.py
+++ tests/test_util.py
@@ -6,7 +6,7 @@ __copyright__ = "Copyright 2013, The QIIME project"
 __credits__ = ["John Chase", "Greg Caporaso", "Jai Ram Rideout",
     "Yoshiki Vazquez-Baeza"]
 __license__ = "GPL"
-__version__ = "0.0.0-dev"
+__version__ = "0.1.0"
 __maintainer__ = "John Chase"
 __email__ = "jc33@nau.edu"
 
